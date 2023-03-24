83089bc4bbb35e6bb6d45ddcbbdf64366548e921
lennardmg <lennardmg@gmx.de>
2023-03-24 14:35:32 +0100

    new task done

--- JavaScript/kyu8.js
+++ JavaScript/kyu8.js
@@ -7,6 +7,8 @@ function booleanToString(b) {
 // Alternative solutions:
 const booleanToString = (b) => (b ? "true" : "false");
 
+// or:
+
 function booleanToString(b) {
     return `${b}`;
 }
@@ -28,10 +30,48 @@ function findNeedle(array) {
     }
 }
 
+// or:
+
 function findNeedle(haystack = []) {
     return haystack.indexOf("needle") !== -1
         ? `found the needle at position ${haystack.indexOf("needle")}`
         : "needle not found";
 }
 
-/// Testcommand
+
+
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+function getGrade (s1, s2, s3) {
+  
+    let gradeAvarage = (s1+s2+s3) / 3
+    
+    if (gradeAvarage >= 90) {
+        return "A"
+        }
+    else if (gradeAvarage >= 80) {
+        return "B"
+        }
+    else if (gradeAvarage >= 70) {
+        return "C"
+        }
+    else if (gradeAvarage >= 60) {
+        return "D"
+        }
+    else if (gradeAvarage < 60) {
+        return "F"
+        }
+  }
+
+  // Alternative solutions:
+
+  function getGrade (s1, s2, s3) {
+    let average = scores.reduce((a, b) => a + b) / scores.length
+    return average >= 90 ? "A" : average >= 80 ? "B" : average >= 70 ? "C" : average >= 60 ? "D" : "F"
+  }
+
+  // or:
+
+  var getGrade=(a,b,c)=>'FFFFFFDCBAA'.charAt((a+b+c)/3/10);
+
